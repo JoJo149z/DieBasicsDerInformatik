239538ca16dc24e6f84adfa935bf5fcae4a7dd1d
JoJo149z <j.mueller.richmann@icloud.com>
2026-01-05 18:36:10 +0100

    Aufgabe03 aber mein bruder ist zu dumm ein test richtig zu schreiben und deswegen muss ich commiten um zu rebasen >:( (nochmal weil hab git add vergessen)

--- Aufgabe03/solution.c
+++ Aufgabe03/solution.c
@@ -3,7 +3,29 @@
 
 int main() {
     int breite = 6;
-    int hoehe = 3;
+    int hoehe = 3; 
+
+    int ih = hoehe;
+    
+    while(ih != 0){
+        printf("A");
+
+        for(int ib = breite; ib != 0; ib--){
+
+            if( ih == hoehe|| ih == 1){
+                printf("A");
+            }else{
+                printf("B");
+            }
+            
+        }
+        printf("A\n");
+         
+
+        ih = ih-1;        
+    }
+
+    
 
     // Hier Code einfügen
 
