afca270a379a9a59fe5412bb5079ac3d2e2d1468
JoJo149z <j.mueller.richmann@icloud.com>
2026-01-21 14:16:14 +0100

    Aufgabe 04 fertig :) nochmak

--- Aufgabe04/solution.c
+++ Aufgabe04/solution.c
@@ -3,8 +3,8 @@
 #include "input.h" // Hier binden wir die Bibliothek ein
 
 // Schreibe hier die Funktion "int fibonacci"
-double fibonacci(int n) {
-    if (n <= 1) {
+int fibonacci(int n) {
+    if (n <= 2) {
         return 1;
     } 
     return fibonacci(n - 1) + fibonacci(n - 2);
